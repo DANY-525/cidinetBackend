773771437ce2b57b4d10c192e441121da72c5efb
daniel <dany525@gmail.com>
2020-12-20 21:54:43 -0500

    desarrollo user module

--- src/modules/user/controller/user.controller.ts
+++ src/modules/user/controller/user.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post,Body } from '@nestjs/common';
+import { Param } from '@nestjs/common';
+import { Controller, Post,Body,Get,Delete,Put} from '@nestjs/common';
 import { UserService } from '../service/user.service';
 
 @Controller('user')
@@ -13,6 +14,40 @@ async create(@Body() newUser:any){
     return await this.userService.create(newUser);
     //return newUser;
 }
+
+
+@Get()
+async getUsers(){
+    return await this.userService.getUsers();
+    //return newUser;
+}
+
+
+
+@Get(':id')
+async getUserById(@Param('id') id){
+    return await this.userService.getUserById(id);
+    //return newUser;
+}
+
+@Delete(':id')
+
+async delete(@Param('id') id){
+
+    console.log(id+"id pasado")
+    return await this.userService.delete(id);
+
+} 
+
+@Put()
+
+async update(@Body() user:any){
+
+  
+    return await this.userService.update(user);
+
+} 
+
 
 
 
--- src/modules/user/service/user.service.ts
+++ src/modules/user/service/user.service.ts
@@ -7,22 +7,38 @@ import { User } from '../model/user.entity';
 
 @Injectable()
 export class UserService {
-
     constructor(@InjectRepository(User) private userRepository:Repository<User>){
 
-
     }
+    async update(user: any) {
+        return await this.userRepository.save(user.id);
+    }
+   
+
+    async delete(id) {
+       return await this.userRepository.delete(id);
+    }
+   
+
+    async getUsers() {
+
+        return await this.userRepository.find();
+    }
+
+
+    async getUserById(id) {
+        return await this.userRepository.findOne(id);
+    }
+
    
    async create(newUser) {
 
-
-
-        return await this.userRepository.save(newUser);
+     return await this.userRepository.save(newUser);
 
     }
 
 
 
 
-    
+
 }
